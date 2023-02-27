c445b1c24c48d4093fe2787b7e76ca198209d701
Shitanshu <shitanshugupta98@gmail.com>
2023-02-27 17:56:25 +0530

    client/server connect/disconnect event

--- server/server.js
+++ server/server.js
@@ -2,4 +2,26 @@ const express = require("express");
 const path = require("path");
 const publicpath = path.join(__dirname, "../public");
 const app = express();
+const { createServer } = require("http")
+const server = require("socket.io");
 app.use(express.static(publicpath));
+
+const PORT = process.env.PORT || 8000
+
+const httpServer = createServer(app)
+const io = server(httpServer) // give acces to the socket.io library
+
+
+io.on("connection", (socket) => {
+    console.log(`connected to the client with Socket id ${socket.id}`);
+    // every time the browser loads it send a request to the server over here.
+
+    socket.on("disconnect", () => {
+        console.log(`Disconnected from the client `);  // every time the browser loads it send a request to the server over here.
+    });
+});
+
+
+httpServer.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+})
