0b425ec73bbc4eda9259e8e45fd671ce593d3213
Shitanshu <shitanshugupta98@gmail.com>
2023-03-01 15:33:46 +0530

    modified some changes

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+let socket = io(); //send a request to the server // io is a method in that library
+socket.on("connect", () => {
+    console.log(`connected to the server with Socket id ${socket.id}`);
+
+    // socket.emit("createMessage", ({
+    //     from: "APPVENTUREZ",
+    //     Date: Date.now()
+    // }))
+
+    socket.on("newMessage", (message) => {
+        console.log(`New Message From server ${JSON.stringify(message)}`);
+    })
+})
+socket.on("disconnect", () => {
+    console.log("disconected from the server ");
+})
--- server/server.js
+++ server/server.js
@@ -16,11 +16,22 @@ io.on("connection", (socket) => {
     console.log(`connected to the client with Socket id ${socket.id}`);
     // every time the browser loads it send a request to the server over here.
 
+    socket.on("createMessage", (message) => {  // listen to the message sent from client side 
+        console.log(`Created Message ${JSON.stringify(message)}`);
+        io.emit("newMessage", {
+            from: message.from,
+            Date: message.Date
+        })
+
+    })
+    // socket.emit("newMessage", ({  // emit an event from server to client 
+    //     "from": "Shitanshu CO.",
+    //     Date: Date.now()
+    // }))
     socket.on("disconnect", () => {
         console.log(`Disconnected from the client `);  // every time the browser loads it send a request to the server over here.
     });
 });
-
 
 httpServer.listen(PORT, () => {
     console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
